abd62e421c2770fbde030782da305bda165254ce
gabriele.lana <gabriele.lana@gmail.com>
2020-11-09 16:45:29 +0100

    Inital commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module git.sighup.io/kage/one-A
+
+go 1.15
+
+require github.com/gin-gonic/gin v1.6.3
---
+++ main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+)
+
+func numberFromServiceB(host string) (int, error) {
+	res, err := http.Get(fmt.Sprintf("http://%s/n", host))
+	if err != nil {
+		return 0, err
+	}
+	defer res.Body.Close()
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return 0, err
+	}
+	ret, err := strconv.Atoi(string(body))
+	return ret, err
+}
+
+func main() {
+	router := gin.Default()
+
+	serviceB := os.Getenv("SERVICE_B_HOST")
+	if serviceB == "" {
+		panic("Missing SERVICE_B_HOST environment variable")
+	}
+
+	router.GET("/do-stuff", func(c *gin.Context) {
+		n, err := numberFromServiceB(serviceB)
+		if err != nil {
+			c.String(http.StatusOK, fmt.Sprint(n))
+		} else {
+			c.String(http.StatusInternalServerError, "Sorry... something went wrong")
+		}
+	})
+
+	router.Run(":8080")
+}
